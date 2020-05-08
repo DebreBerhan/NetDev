a5afdf587b05573fba6c8c61831f8dd971daf53c
DebreBerhan <tedla.shiferaw@gmail.com>
2020-05-08 12:02:55 -0400

    rev 1

---
+++ Meskel/Meskel/Data/MeskelContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Meskel.Models;
+
+namespace Meskel.Data
+{
+    public class MeskelContext : DbContext
+    {
+        public MeskelContext (DbContextOptions<MeskelContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Meskel.Models.Product> Product { get; set; }
+    }
+}
---
+++ Meskel/Meskel/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Meskel.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Color { get; set; }
+        public string pp_image { get; set; }
+        public double Price { get; set; }
+    }
+}
